711f1eba3525bdfecdfcb6efa825fc42010c249c
Aleksandr Petrov <Aleksandr Petrov>
2016-07-28 16:47:10 +0700

    few refactor extend env function to use default values

--- config/service.php
+++ config/service.php
@@ -4,12 +4,11 @@ $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
 
 $config = [
-    'timeZone'            => 'Europe/Moscow',
     'bootstrap'           => ['log'],
     'defaultRoute'        => 'main',
     'controllerNamespace' => 'app\commands',
     'components'          => [
-        'log' => [
+        'log' => [ //TODO: temporarily before creating log service you can overload this in your service config
             'flushInterval' => 1,
             'traceLevel'    => YII_DEBUG ? 3 : 0,
             'targets'       => [
@@ -32,10 +31,10 @@ $config = [
                     'levels'         => ['error', 'warning'],
                     'exportInterval' => 1,
                     'username'       => 'SERVICE | PROMO',
-                    'viewErrorUrl'   => '@backendUrl/log/error/view',
+                    'viewErrorUrl'   => '@backendUrl/log/error/view', //TODO: overload in your service config
                     'channel'        => env('SLACK_MONITORING_CHANNEL_OPTIONAL'),
                     'icon'           => env('SLACK_MONITORING_ICON_OPTIONAL'),
-                    'endpoint'       => env('SLACK_MONITORING_ENDPOINT', (boolean)env('SLACK_MONITORING_DISABLED_OPTIONAL'))
+                    'endpoint'       => env('SLACK_MONITORING_ENDPOINT', (boolean)env('SLACK_MONITORING_DISABLED_OPTIONAL') ? '' : NULL)
                 ]
             ],
         ],
--- src/helpers/service-functions.php
+++ src/helpers/service-functions.php
@@ -3,17 +3,34 @@
 /**
  * Get environment variable
  *
- * @param      $varName
- * @param bool $optional
+ * @param       $varName
+ * @param mixed $default
  * @return string
- * @throws Exception if there is no not optional variable
+ * @throws Exception if there is no not optional variable and no default value
  */
-function env($varName, $optional = false)
-{
-    $env = getenv($varName);
-    if ($env !== false || $optional || stripos($varName, 'optional') !== false) {
+if (!function_exists('env')) {
+    function env($varName, $default = NULL)
+    {
+        $env = getenv($varName);
+        if ($env === false && !is_null($default)) {
+            return $default;
+        } elseif ($env === false && is_null($default) && stripos($varName, 'optional') === false) {
+            throw new Exception("UNABLE TO FIND NECESSARY ENV VARIABLE: $varName");
+        }
+        switch (strtolower($env)) {
+            case 'true':
+            case '(true)':
+            case 'on':
+                return true;
+            case 'false':
+            case '(false)':
+            case 'off':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+        }
+
         return $env;
     }
-
-    throw new Exception("Unable to find necessary env variable $varName");
 }
